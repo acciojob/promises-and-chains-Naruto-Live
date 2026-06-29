b2c187eb6bc92162e6b98bd99c3ec65db8760987
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-29 16:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your JS code here. If required.
+const form = document.getElementById("form");
+const age = document.getElementById("age");
+const name = document.getElementById("name");
+
+form.addEventListener("submit", function (e) {
+
+    // Prevent page refresh
+    e.preventDefault();
+
+    const userAge = age.value;
+    const userName = name.value;
+
+    // Validation
+    if (userAge === "" || userName === "") {
+        alert("Please enter valid details.");
+        return;
+    }
+
+    const promise = new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            if (Number(userAge) > 18) {
+                resolve();
+            } else {
+                reject();
+            }
+
+        }, 4000);
+
+    });
+
+    promise
+        .then(() => {
+            alert(`Welcome, ${userName}. You can vote.`);
+        })
+        .catch(() => {
+            alert(`Oh sorry ${userName}. You aren't old enough.`);
+        });
+
+});
